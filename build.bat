3121a20383e640c99aeec87e960a852d9a4d0e85
Victor Greggio <victor_greggio@hotmail.com>
2025-07-07 15:41:01 -0500

    Fixing path to VCPKG cmake

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake
+cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake"
 cmake --build build
